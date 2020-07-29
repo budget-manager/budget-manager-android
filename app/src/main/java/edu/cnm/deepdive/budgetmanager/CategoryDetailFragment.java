cf180c5f305bd5b49ecc63ed1bb509a8f1af83c1
Mercedes <mercedes.nmx@gmail.com>
2020-07-29 16:14:44 -0600

    Created navigation.xml

---
+++ app/src/main/java/edu/cnm/deepdive/budgetmanager/CategoryDetailFragment.java
@@ -0,0 +1,26 @@
+package edu.cnm.deepdive.budgetmanager;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CategoryDetailFragment extends Fragment {
+
+  public CategoryDetailFragment() {
+    // Required empty public constructor
+  }
+
+
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+      Bundle savedInstanceState) {
+    // Inflate the layout for this fragment
+    return inflater.inflate(R.layout.fragment_category_detail, container, false);
+  }
+}
